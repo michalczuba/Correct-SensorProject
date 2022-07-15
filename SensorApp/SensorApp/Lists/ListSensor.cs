5b83916862f1d782f0cd4c486abefe8fce15ab48
SKK\jakub.mrozowski <jakub.mrozowski@skkglobal.com>
2022-07-15 12:09:23 +0200

    Adding program to read csv files and making list of it

---
+++ SensorApp/SensorApp/Lists/ListSensor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  CommonStaff.Moduls;
+using SensorApp.Servis;
+namespace SensorApp.Lists
+{
+    internal class ListSensor
+    {
+        public List<SensorModel> SensorList { private set; get; } = new List<SensorModel>();
+        public void ReadFromFile(string name)
+        {
+            SensorList = new List<SensorModel>();
+            SensorList = csvOpener.Open(name);
+        }
+    }
+}
