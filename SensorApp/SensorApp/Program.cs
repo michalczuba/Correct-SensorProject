e0996ad6adc9a7a328c8c5d6009c95d6a0231ccc
SKK\jakub.mrozowski <jakub.mrozowski@skkglobal.com>
2022-07-15 12:33:49 +0200

    Add a lane to strat blescan and making list of result

--- SensorApp/SensorApp/Program.cs
+++ SensorApp/SensorApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using SensorApp.Lists;
-
+using CommonStaff;
 string name;
 name = Console.ReadLine();
 string filename = name;
 var listSensor = new ListSensor();
 listSensor.ReadFromFile(filename);
+var com = LinuxCommand.SystemCommand("sudo blescan");
+var listBluetooth = new ListBluetooth();
+listBluetooth.ReadBlue(com);
 
+        
