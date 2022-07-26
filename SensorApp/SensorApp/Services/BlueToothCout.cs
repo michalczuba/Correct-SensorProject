e85174a0f0217d8dc0451d9f4a4ddc28c25a8715
SKK\jakub.mrozowski <jakub.mrozowski@skkglobal.com>
2022-07-26 13:38:29 +0200

    Addint BlueToothCout

---
+++ SensorApp/SensorApp/Services/BlueToothCout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Modeles;
+namespace SensorApp.Services
+{
+    public class BlueToothCout
+    {
+        public static void CoutBlueToothList(List<BleDeviceModel> ListBlueTooth)
+        {
+            foreach(var val in ListBlueTooth)
+            {
+                string output = val.Mac+" "+val.DBm.ToString()+" ";
+                foreach(var value in val.Manufacture)
+                {
+                    output += value.ToString()+",";
+                }
+                int output_size =output.Length;
+                output =output.Substring(0,output_size-1);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
