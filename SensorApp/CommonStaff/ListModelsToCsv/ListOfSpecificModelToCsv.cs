95aafe7f12d53ece31b13aac4b1c949a26021c67
SKK\jakub.mrozowski <jakub.mrozowski@skkglobal.com>
2022-08-12 10:16:32 +0200

    Better ListOfSpecificModelToCsv

--- SensorApp/CommonStaff/ListModelsToCsv/ListOfSpecificModelToCsv.cs
+++ SensorApp/CommonStaff/ListModelsToCsv/ListOfSpecificModelToCsv.cs
@@ -15,65 +15,85 @@ namespace Common.ListModelsToModelCsv
             public string WarningAdv { get; set; }
             public string Pagaes_per { get; set; }
         }
-        public static void ListOfBleDeviceModelToCsvFile(IEnumerable<BleDeviceModel> list, List<SensorModel> CsvFile, double Rw,double AdvUp,double AdvDown,int IndexAdv,double pakages)
+        public class PackagesStatistic
+        {
+            public double SumOfPackagesPercent { get; set; }
+            public int NumberOfDevicesScanned { get; set; }
+        }
+
+        public static void ListOfBleDeviceModelToCsvFile(IEnumerable<BleDeviceModel> list, List<SensorModel> CsvFile, double Rw, double AdvUp, double AdvDown, int IndexAdv, double pakages)
         {
             var ListPB = new List<PartialBleDiviceModel>();
             List<BleDeviceModel> TmpList = list.ToList();
-            double Avrege_PP = 0;
-            int tmp = 0;
-            foreach (var val in CsvFile)
-            {
-                PartialBleDiviceModel PB = new PartialBleDiviceModel();
-                int index = TmpList.FindIndex(x => x.Mac.Equals(val.Mac, StringComparison.OrdinalIgnoreCase));
-                if (index == -1)
-                {
-                    PB.Mac = val.Mac;
-                    PB.SerialNumber = val.SerialNumber;
-                    PB.Rssi = null;
-                    PB.WarningOrMissing = "Missing";
-                }
-                else
-                {
-                    double check = (TmpList[index].DBm.Count / pakages)*100;
-                    Avrege_PP += check;
-                    tmp++;
-                    PB.Pagaes_per = check.ToString("0.##") + "%";
-                    PB.Mac = val.Mac;
-                    PB.SerialNumber = val.SerialNumber;
-                    PB.Rssi = TmpList[index].Avrege;
-                    if (PB.Rssi < Rw)
-                        PB.WarningOrMissing = "Warning";
-                    else
-                        PB.WarningOrMissing = "";
-
-                    if(TmpList[index].Manufacture.Count()>= IndexAdv)
-                    {
-                        if (TmpList[index].Manufacture.ElementAt(IndexAdv - 1) > AdvUp || TmpList[index].Manufacture.ElementAt(IndexAdv - 1) < AdvDown)
-                        {
-                            PB.WarningAdv = "WarningAdv";
-                        }
-                    }
-                    else
-                    {
-                        PB.WarningAdv = "WarningAdv";
-                    }
-                    
-
-                }
-
-
-                ListPB.Add(PB);
-
-            }
-            Avrege_PP /= tmp;
+            ListPB = CreateAListToSave(CsvFile, TmpList, Rw, AdvUp, AdvDown, IndexAdv, pakages);
             //Uncoment line below to see avrege package per
-            Console.WriteLine("Avrega PP: " + Avrege_PP.ToString("0.##") + "%");
+            //Console.WriteLine("Avrega PP: " + Avrege_PP.ToString("0.##") + "%");
             using (var writer = new StreamWriter("ScannedBleDeviceModelFile.csv"))
             using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
             {
                 csv.WriteRecords(ListPB);
             }
         }
+        public static PartialBleDiviceModel CreataPartialDevice(SensorModel val, List<BleDeviceModel> TemporaryListOfBleDeviceModel, int index, double Rw, double AdvUp, double AdvDown, int IndexAdv, double packages)
+        {
+            PartialBleDiviceModel partialBleDeviceModel = new PartialBleDiviceModel();
+            if (index == -1)
+            {
+                partialBleDeviceModel.Mac = val.Mac;
+                partialBleDeviceModel.SerialNumber = val.SerialNumber;
+                partialBleDeviceModel.Rssi = null;
+                partialBleDeviceModel.WarningOrMissing = "Missing";
+            }
+            else
+            {
+                //Calulating percent of package 
+                double check = (TemporaryListOfBleDeviceModel[index].DBm.Count / packages) * 100;
+                partialBleDeviceModel.Pagaes_per = check.ToString("0.##") + "%";
+                partialBleDeviceModel.Mac = val.Mac;
+                partialBleDeviceModel.SerialNumber = val.SerialNumber;
+                partialBleDeviceModel.Rssi = TemporaryListOfBleDeviceModel[index].Avrege;
+                if (partialBleDeviceModel.Rssi < Rw)
+                    partialBleDeviceModel.WarningOrMissing = "Warning";
+                else
+                    partialBleDeviceModel.WarningOrMissing = "";
 
+                if (TemporaryListOfBleDeviceModel[index].Manufacture.Count() >= IndexAdv)
+                {
+                    if (TemporaryListOfBleDeviceModel[index].Manufacture.ElementAt(IndexAdv - 1) > AdvUp || TemporaryListOfBleDeviceModel[index].Manufacture.ElementAt(IndexAdv - 1) < AdvDown)
+                    {
+                        partialBleDeviceModel.WarningAdv = "WarningAdv";
+                    }
+                }
+                else
+                {
+                    partialBleDeviceModel.WarningAdv = "WarningAdv";
+                }
+
+
+            }
+            return partialBleDeviceModel;
+        }
+        public static List<PartialBleDiviceModel> CreateAListToSave(List<SensorModel> CsvFile, List<BleDeviceModel> TemporaryListOfBleDeviceModel, double Rw, double AdvUp, double AdvDown, int IndexAdv, double packages)
+        {
+            List<PartialBleDiviceModel> ListOfPartialDeviceModel = new List<PartialBleDiviceModel>();
+            PackagesStatistic AvregePackagesPercent = new PackagesStatistic();
+            foreach (var val in CsvFile)
+            {
+                int index = TemporaryListOfBleDeviceModel.FindIndex(x => x.Mac.Equals(val.Mac, StringComparison.OrdinalIgnoreCase));
+                ListOfPartialDeviceModel.Add(CreataPartialDevice(val, TemporaryListOfBleDeviceModel, index, Rw, AdvUp, AdvDown, IndexAdv, packages));
+                if (index != -1)
+                {
+                    double tmp = double.Parse(ListOfPartialDeviceModel[ListOfPartialDeviceModel.Count - 1].Pagaes_per.Substring(0, ListOfPartialDeviceModel[ListOfPartialDeviceModel.Count - 1].Pagaes_per.Length - 1)) / 100;
+                    AvregePackagesPercent.SumOfPackagesPercent += tmp;
+                    AvregePackagesPercent.NumberOfDevicesScanned++;
+                }
+            }
+            WriteAvregePackagesPercent((AvregePackagesPercent.SumOfPackagesPercent / AvregePackagesPercent.NumberOfDevicesScanned)*100);
+            return ListOfPartialDeviceModel;
+        }
+        public static void WriteAvregePackagesPercent(double avrege)
+        {
+            Console.WriteLine("Avrega PP: " + avrege.ToString("0.##") + "%");
+        }
     }
 }
